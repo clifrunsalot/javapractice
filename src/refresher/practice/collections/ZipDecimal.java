f7fab5beb22b6d6e185e21abd882a740df6ae755
Clif Hudson <clifrunsalot@yahoo.com>
2017-01-15 15:01:33 -0500

    Added try/catch to ZipDecimal.java

--- src/refresher/practice/collections/ZipDecimal.java
+++ src/refresher/practice/collections/ZipDecimal.java
@@ -2,7 +2,11 @@ package refresher.practice.collections;
 
 import refresher.practice.helpers.LogIt;
 
+import java.util.logging.Level;
+
 public class ZipDecimal {
+
+    static LogIt logger = new LogIt(ZipDecimal.class.getName());
 
     /**
      * Returns a new decimal representing the merge between A and B.
@@ -16,44 +20,60 @@ public class ZipDecimal {
      */
     private static int zipIt(int A, int B) {
 
-        char[] aAry = Integer.toString(A).toCharArray();
-        char[] bAry = Integer.toString(B).toCharArray();
-
-        int totalLen = aAry.length + bAry.length;
-
-        int aIdx = 0;
-        int bIdx = 0;
-
+        int result = -1;
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < totalLen; i++) {
+        try {
 
-            if (aIdx < aAry.length) {
+            if (A > 0 && B > 0) {
 
-                sb.append(aAry[aIdx]);
-                aIdx += 1;
+                char[] aAry = Integer.toString(A).toCharArray();
+                char[] bAry = Integer.toString(B).toCharArray();
 
+                int totalLen = aAry.length + bAry.length;
+
+                int aIdx = 0;
+                int bIdx = 0;
+
+                for (int i = 0; i < totalLen; i++) {
+
+                    if (aIdx < aAry.length) {
+
+                        sb.append(aAry[aIdx]);
+                        aIdx += 1;
+
+                    }
+
+                    if (bIdx < bAry.length) {
+
+                        sb.append(bAry[bIdx]);
+                        bIdx += 1;
+
+                    }
+
+                    if (sb.length() == totalLen) {
+                        break;
+                    }
+                }
+
+                result = Integer.parseInt(sb.toString());
+
+            } else {
+
+                logger.getLogger().log(Level.SEVERE,"Arguments must be > 0");
             }
 
-            if (bIdx < bAry.length) {
+        } catch (IllegalArgumentException iae) {
 
-                sb.append(bAry[bIdx]);
-                bIdx += 1;
+            logger.getLogger().log(Level.SEVERE, "Zipped value exceeds capacity of an int type. Use smaller values.");
 
-            }
-
-            if (sb.length() == totalLen) {
-                break;
-            }
         }
 
-        return Integer.parseInt(sb.toString());
+        return result;
 
     }
 
     public static void main(String[] args) {
-
-        LogIt logger = new LogIt(ZipDecimal.class.getName());
 
         int A = 53;
         int B = 53;
@@ -77,6 +97,18 @@ public class ZipDecimal {
 
         A = 1;
         B = 2;
+        logger.logFunc("zipIt", A + "," + B, "" + zipIt(A, B));
+
+        A = 999999999;
+        B = 999999999;
+        logger.logFunc("zipIt", A + "," + B, "" + zipIt(A, B));
+
+        A = 99999;
+        B = 9999;
+        logger.logFunc("zipIt", A + "," + B, "" + zipIt(A, B));
+
+        A = -99999;
+        B = 9999;
         logger.logFunc("zipIt", A + "," + B, "" + zipIt(A, B));
     }
 
